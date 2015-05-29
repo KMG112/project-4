5077521de59ec65c87ef1ed28a1ddaed3905a27c
KMG112 <mattwuertz@gmail.com>
2015-05-29 16:30:15 -0400

    added drag and drop + centering

--- WordApp/app/controllers/sentences_controller.rb
+++ WordApp/app/controllers/sentences_controller.rb
@@ -1,6 +1,6 @@
 class SentencesController < ApplicationController
   before_action :set_sentence, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
   # GET /sentences
   # GET /sentences.json
   def index
