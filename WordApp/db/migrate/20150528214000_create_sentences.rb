356a7ae9bd3ed881415d471cdcfced44064c218e
KMG112 <mattwuertz@gmail.com>
2015-05-30 17:52:16 -0400

    multiple drop locations work

--- WordApp/db/migrate/20150528214000_create_sentences.rb
+++ WordApp/db/migrate/20150528214000_create_sentences.rb
@@ -1,12 +1,10 @@
 class CreateSentences < ActiveRecord::Migration
   def change
     create_table :sentences do |t|
-    	t.string :word1
-    	t.string :word2
-    	t.string :word3
-    	t.string :word4
-    	t.string :word5
+      # t.belongs_to :user
+    	t.string 'words', array: true, default: []
       t.timestamps null: false
     end
+    add_index :sentences, :words, using: 'gin'
   end
 end
