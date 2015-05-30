c8fa3d71c57c02e0d6f8edcb1b116fe542da6939
KMG112 <mattwuertz@gmail.com>
2015-05-30 15:33:19 -0400

    added multiple boxes, boxes are not working correctly

--- WordApp/app/assets/javascripts/sentences.js
+++ WordApp/app/assets/javascripts/sentences.js
@@ -1,25 +1,29 @@
 $(document).ready(function(){
 
+  
+  	$('#box1,#box2,#box3,#box4,#box5').draggable({
+  		revert: "invalid"
+  	});
+  	
+  	$('#droppable1,#droppable2,#droppable3,#droppable4, #droppable5').droppable({
+  		drop: function( event, ui ) {
+  			console.log(this)
+          $( this ).addClass( "ui-state-highlight" )
+            .find( "p" )
+              .html( "Dropped!" );
 
-	$('#box').draggable({
-		revert: "invalid"
-	});
-	
-	$('#droppable').droppable({
-		drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Dropped!" );
+          $('#box1,#box2,#box3,#box4,#box5').position({
+          		
+          	  of: $('#droppable1,#droppable2,#droppable3,#droppable4, #droppable5'),
+              my: "center"      	  
+          	          })
+          	 	 
+        }
+  	})
+  
 
-        $('#box').position({
-        	of: $('#droppable'),
-        	my: "center"
 
-        })
-      }
 
-	})
 
 
 });
