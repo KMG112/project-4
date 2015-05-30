5c31b747bbb61476becbd4a2d42c46f69a6c3dda
KMG112 <mattwuertz@gmail.com>
2015-05-30 17:52:16 -0400

    multiple drop locations work

--- app/assets/javascripts/sentences.js
+++ app/assets/javascripts/sentences.js
@@ -1,29 +1,50 @@
 $(document).ready(function(){
+  
+	var createDroppableObject = function(){
+  	  	$('#sentence_parts').append("<div id=droppable class='ui-widget-header'></div>")
+  	}
+  	createDroppableObject();
+
+	// creating draggable objects with for statement
+	// objects created will get value from the position they are in, 
+	// in the active record array
+	
+	for(var i=0; i<=5; i++){
+		$('#words').append("<div id=box"+i+" class='ui-widget-header'></div>");
+	
+		$('#box'+i).draggable({
+  			revert: "invalid"
+  		});
+  		
+  		$('#box'+i).on('drag', function(){
+  			makeDroppable(this);
+  		});
+	}
 
   
-  	$('#box1,#box2,#box3,#box4,#box5').draggable({
-  		revert: "invalid"
-  	});
-  	
-  	$('#droppable1,#droppable2,#droppable3,#droppable4, #droppable5').droppable({
-  		drop: function( event, ui ) {
-  			console.log(this)
-          $( this ).addClass( "ui-state-highlight" )
-            .find( "p" )
-              .html( "Dropped!" );
+	// function to make only selected item dropped into droppable div    
+	var makeDroppable = function(box){
 
-          $('#box1,#box2,#box3,#box4,#box5').position({
-          		
-          	  of: $('#droppable1,#droppable2,#droppable3,#droppable4, #droppable5'),
-              my: "center"      	  
-          	          })
-          	 	 
-        }
-  	})
+    	$('#droppable').droppable({
+
+    		drop: function( event, ui ) {
+            $( this ).addClass( "ui-state-highlight" );                 		
+            $(box).position({
+            	  of: $('#droppable'),
+                  my: "center"      	  
+            	          });
+            $(this).removeAttr('id');
+  			$(this).attr('id', 'dropped');
+            $(box).off();
+            $( this ).off();
+            createDroppableObject();	 	 
+          }
+    	})
+    
   
+  	} //make droppable
 
 
-
-
+  
 
 });
