cc9ba634e042b4cc6655744b679f7973837c02a4
KMG112 <mattwuertz@gmail.com>
2015-05-31 15:19:14 -0400

    sentence refesh works

--- WordApp/app/assets/javascripts/sentences.js
+++ WordApp/app/assets/javascripts/sentences.js
@@ -1,4 +1,4 @@
- 
+
 $(document).ready(function(){
 
 	var createDroppableObject = function(){
@@ -10,28 +10,31 @@ $(document).ready(function(){
 	// creating draggable objects with for statement
 	// objects created will get value from the position they are in, 
 	// in the active record array
-
-	for(var i=0; i<5; i++){
-		$('#words').append("<div id=box"+i+" class='ui-widget-header'></div>");
-		
-
-		$('#box'+i).draggable({
-  			revert: "invalid"
-  		});
-  		
-  		$('#box'+i).on('drag', function(){
-  			makeDroppable(this);
-  		});
- 
-	}// for loop end
-
-  
+	var createDrag = function(){
+		$('#words').remove();
+		$('body').append('<div id="words"></div>')
+			for(var i=0; i<5; i++){
+				$('#words').append("<div id=box"+i+" class='ui-widget-header'></div>");
+				
+	
+				$('#box'+i).draggable({
+		  			revert: "invalid"
+		  		});
+		  		
+		  		$('#box'+i).on('drag', function(){
+		  			makeDroppable(this);
+		  		});
+		 
+			}// for loop end
+	}//createDrag ends
+  	createDrag();
 	// function to make only selected item dropped into droppable div    
 	var makeDroppable = function(box){
 
     	$('#droppable').droppable({
 
     		drop: function( event, ui ) {
+    			
             $( this ).addClass( "ui-state-highlight" );                 		
             $(box).position({
             	  of: $('#droppable'),
@@ -39,17 +42,35 @@ $(document).ready(function(){
             	          });
             $(this).removeAttr('id');
   			$(this).attr('id', 'dropped');
+  			$(this).text($(box).text())
             $(box).off();
             $( this ).off();
             createDroppableObject();	 	 
+             // getting data from database
+		    createDrag();
+		    $.ajax({
+			    type: "GET",
+			    url: "http://localhost:3000/sentences/1",
+			    dataType: "json",
+			    success: function(data) {
+			    	console.log();
+			      for(var j=0; j<data.content.length; j++){	       
+			      	 $('draggable').text($(box).text());
+			      	};
+			    }//seccess end
+			 
+		 	});//ajax end
+
           }
+         
+
     	})
     
   
   	} //make droppable end
 
   	// getting data from database
-	 var ajax=	$.ajax({
+	$.ajax({
 		    type: "GET",
 		    url: "http://localhost:3000/sentences/1",
 		    dataType: "json",
